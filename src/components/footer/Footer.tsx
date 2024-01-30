3a903a41ba31e363b5ecedc1943c6fb4541ec8b6
Theophilus E.K Adukpo <theoadu@gmail.com>
2024-01-30 17:47:49 +0000

    add Footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './footer.scss';
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <span>AVEMMED</span>
+      <span>© AVEMMED Dev Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
